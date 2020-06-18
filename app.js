388601d1b6e7552ee5f1d70f9c9a05e1daf86bc7
Robin Bolscher <robinbolscher@gmail.com>
2020-06-18 16:50:19 +0200

    chore: disable homey-log

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line node/no-unpublished-require
 const Homey = require('homey');
 // eslint-disable-next-line no-unused-vars
-const { Log } = require('homey-log');
+// const { Log } = require('homey-log'); // TODO: re-enable when SDKv3 compatible
 
 // Enable zigbee-cluster logging
 const { Util } = require('homey-zigbeedriver');
